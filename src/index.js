1f259143e424b1736b30036c9d7e692a0c5c6e22
maria-kashpur <kashpur.maria@icloud.com>
2023-02-16 10:08:56 +0300

    feat: add function decode(expr)

--- src/index.js
+++ src/index.js
@@ -39,6 +39,21 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let morse = []
+   for (let i = 0; i < expr.length; i += 10) {
+      let letter = expr.slice(i, i+10);
+      letter === '**********' ? letter = " " : letter = String(Number(letter))
+      let letterMorse = ''
+      for (let l = 0; l < letter.length; l += 2){
+         (letter.slice(l, l + 2)) === "10" ? letterMorse += '.' : (letter.slice(l, l + 2)) === "11" ? letterMorse += '-' : letterMorse += " ";
+      }
+      morse.push(letterMorse)
+   }
+   return morse = morse
+   .map(el => {
+      return el === " " ? el : el =  MORSE_TABLE[el]
+   })
+   .join("")
 }
 
 module.exports = {
